fcb2054f75c1f80c7a49f6ac43278ebe1f23f38e
Carmen-digitalPebble <DigitalPebble.Carmen@gmail.com>
2013-02-21 15:02:45 +0100

    FastICA based on Gaevert etc.

--- code/FastICAC/fastICA.m
+++ code/FastICAC/fastICA.m
@@ -1,4 +1,19 @@
 
+
+
+%based on FastICA package of Matlab 7.x and 6.x
+  % Version 2.5, October 19 2005 Copyright (c) Hugo Gävert, Jarmo Hurri, Jaakko Särelä, and Aapo Hyvärinen.
+   %This program is free software: you can redistribute it and/or modify
+   % it under the terms of the GNU General Public License as published by
+   % the Free Software Foundation, either version 3 of the License, or
+   % (at your option) any later version.
+
+    %This program is distributed in the hope that it will be useful,
+    %but WITHOUT ANY WARRANTY; without even the implied warranty of
+    %MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    %GNU General Public License for more details.
+
+
 function [A,W] = fastICA(mixedsig, no_comp) % takes as input mixing matrix (document-term! matrix) and desired no- of components 
 
 
