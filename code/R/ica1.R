7d7fd923f4cc3345d38d934c77e4e9993b7832fc
Carmen-digitalPebble <DigitalPebble.Carmen@gmail.com>
2013-02-27 18:38:28 +0100

    First Version R Preprocessing

---
+++ code/R/ica1.R
@@ -0,0 +1,82 @@
+library(tm)                        
+
+
+# set parameters
+#sparsity = "1.0"
+
+args <- commandArgs(TRUE)
+loc <- (args[1])    # get corpus location
+
+
+# create document-term matrix with prepocessing 
+
+corpus <- function(corpusLoc) {
+
+  print("...creating Text corpus...")
+ 
+ (docCorpus <- Corpus(DirSource(loc),readerControl = list(language = "en")))   # create corpus from source
+
+ return(docCorpus)
+ }
+ 
+ 
+ prep <- function(textCorpus){
+ 
+      print("...prepocessing, - to lower case, removing Numbers and Punctuation...")
+      
+    textCorpus <- tm_map(textCorpus, stripWhitespace)
+    textCorpus  <- tm_map(textCorpus, tolower)
+
+    textCorpus <- tm_map(textCorpus,removePunctuation)
+    textCorpus <- tm_map(textCorpus,removeNumbers)
+    
+    return(textCorpus)
+}
+
+
+dtm <- function(textCorpus){
+
+print("...creating Document-by-Term Matrix...")
+
+docTermM <- DocumentTermMatrix(textCorpus)
+return(docTermM)
+}
+
+
+rmvSparsity <- function(dtm,sparsity){
+
+print("removing sparse terms")
+ spDtm <- removeSparseTerms(dtm, sparsity)
+ 
+ return(spDtm)
+}
+
+# ---------------------------------------------------------------------------------------------------
+
+
+rmvMean <- function(dtm){
+
+
+ featMeans <- colMeans(dtm)
+ 
+ 
+
+
+}
+
+
+
+# Main
+
+y <- corpus(loc);
+prepText <- prep(y)
+dtm <- dtm(prepText)
+
+
+
+
+
+
+
+
+
