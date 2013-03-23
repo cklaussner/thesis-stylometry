a85b2c756d6cb38ae21998da8b7e0a9391aeae36
Carmen-digitalPebble <DigitalPebble.Carmen@gmail.com>
2013-03-23 17:27:38 +0100

    draft extract keywords

--- code/R/style.R
+++ code/R/style.R
@@ -1,6 +1,6 @@
 library(tm)  
 library (plyr)
-
+library("slam")
 
 source("preprocessing/prepMat.R")
 source("ica/rmvMean.R")
@@ -27,18 +27,105 @@ spDtm <- removeSparseTerms(dtm, 0.4) # 2nd argument indication of sparsity in ma
 
 #############   centering  ############
 
-dtmCent <- rmvMean(spDtm)
-mixedMean <- getFeatureMean()
+#dtmCent <- rmvMean(spDtm)
+#mixedMean <- getFeatureMean()
 
 ############ whitening ############## 
 
-pc <- svdMat(dtmCent)  # calculate new components with svd 
-xwhiten <- getwhiteningMatrix()
+#pc <- svdMat(dtmCent)  # calculate new components with svd 
+#xwhiten <- getwhiteningMatrix()
 
 #pc <-pcamat(dtmCent) # also works, but svd gives better approx.
 ############# ica ###################
 
 
-ics <- ica(pc,xwhiten, 4)
-icasig <- calcIC(spDtm,ics,mixedMean)
+#ics <- ica(pc,xwhiten, 4)
+#icasig <- calcIC(spDtm,ics,mixedMean)
+
+numOfIC <- 85   # set no. of comp
+
+
+Y<- fastICA(nV, numOfIC, alg.typ = "deflation",
+         fun = "exp", alpha = 1.0, method = "R",
+           row.norm = TRUE, maxit = 200, tol = 1e-04, verbose = TRUE,
+          w.init = NULL)
+
+S <- Y$S # extract doc-by-comp 
+A <- (Y$A) # get inverse of unmixing matrix W --> weights for terms of each comp.
+
+Asize <- dim(A)
+numOfComps <- Asize[1] 
+numOfTerms <- Asize[2] 
+
+Ssize <- dim(S)
+numOfDocs <- Ssize[1] 
+numOfComps <- Ssize[2] 
+
+compT <- abs(A) # get absolute vals since ica model is ambiguous in regard to sign 
+docs <- abs(S)
+
+colnames(docs) <- paste(1:noOfIC)   # name components
+docnames <- rownames(docs)
+
+colnames(compT) <- colnames(nV[2:length(nV)])  # get term names
+compLst <- list()
+
+#-------------------------create list where for each comp - list of keyword-weight pair 
+
+for(i in 1:numOfIC) {   # sort keywords in components according to weight
+  key <- sort(compT[i, ],decreasing = TRUE) # sort comp. according to most prominent keywords
+  label <- names(key) # get term names
+  names(key) <- NULL # rmv term names
+  lst <- list()
+  
+  for (j in 1:numOfTerms){ # for comp create list key - weight for easy access 
+    lst[[label[j]]] <- key[j]
+    }
+  
+ compLst[[i]] <-  lst     # add to overall comp list
+
+}
+#---------------------------# same for components for each doc - order according to weight - pos in list = pos in doc
+
+docLst <- list()
+for (i in 1:numOfDocs){   # order comp for each document
+  
+  c_ord <- sort(docs[i, ],decreasing = TRUE)  # sort accord. to weight
+
+  label <- names(c_ord) # get comp names
+  names(c_ord) <- NULL # rmv comp names
+  lst <- list()
+  
+  for (j in 1:numOfIC){ 
+    lst[[label[j]]] <- c_ord[j]
+  }
+  
+  docLst[[docnames[i]]] <-  lst     # add to overall comp list
+}
+
+
+#----------------------------------
+
+
+dcomp <- list()
+for (i in 1:numOfDocs){   # get keywords for each document
+  
+  complist <- docLst[i] # get component list for each doc
+  
+  for 
+  
+  
+  c_ord <- sort(c,decreasing = TRUE)
+  nam <- paste("docRank",i , sep = ".")
+  assign(nam, c_ord)
+}
+
+
+
+
+#(sort(col_sums((dtm)),decreasing = TRUE)) 
+
+
+
+
 
